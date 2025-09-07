2179e1d7256ae7662e4922a77641e74ada124858
pk_3 <wotcool1@yandex.ru>
2025-09-07 18:19:03 +0300

    другой пк

---
+++ src/main/java/ru/yandex/practicum/filmorate/dal/UserRepository.java
@@ -0,0 +1,6 @@
+package ru.yandex.practicum.filmorate.dal;
+
+import ru.yandex.practicum.filmorate.model.User;
+
+public class UserRepository  {
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,52 @@
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    login VARCHAR(255) NOT NULL,
+    user_name VARCHAR(255),
+    birthday DATE
+);
+
+
+CREATE TABLE IF NOT EXISTS films (
+    film_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    film_name VARCHAR(255) NOT NULL,
+    description VARCHAR(200),
+    release_date DATE,
+    duration INTEGER
+);
+
+
+CREATE TABLE IF NOT EXISTS genres (
+    genre_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    genre_name VARCHAR(255) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE IF NOT EXISTS film_genre (
+    film_id INTEGER NOT NULL,
+    genre_id INTEGER NOT NULL,
+    PRIMARY KEY (film_id, genre_id),
+    FOREIGN KEY (film_id) REFERENCES films(film_id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genres(genre_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS likes (
+    film_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    PRIMARY KEY (film_id, user_id),
+    FOREIGN KEY (film_id) REFERENCES films(film_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS friendships (
+    user_id INTEGER NOT NULL,
+    friend_id INTEGER NOT NULL,
+    status VARCHAR(20) DEFAULT 'PENDING',
+    PRIMARY KEY (user_id, friend_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (friend_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    CHECK (user_id != friend_id)
+);
