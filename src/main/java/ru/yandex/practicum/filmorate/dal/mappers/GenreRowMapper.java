f77f49b8bd2dce194230f351528639025b36639d
Dmitry Sokolov <wotcool1@yandex.ru>
2025-10-28 19:28:55 +0300

    2 итерация

--- src/main/java/ru/yandex/practicum/filmorate/dal/mappers/GenreRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/mappers/GenreRowMapper.java
@@ -6,8 +6,6 @@ import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class GenreRowMapper implements RowMapper<Genre> {
